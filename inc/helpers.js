b5ec3394fe135ad02c88bc0df34dacbb4621f4f7
Chang Zhao <chang-zhao@users.noreply.github.com>
2019-12-22 21:21:37 +0300

    Update helpers.js

--- inc/helpers.js
+++ inc/helpers.js
@@ -2,9 +2,13 @@ exports.withZero = function (n) {
 	if (n <= 9) return "0" + n;
 	return n
 }
-exports.myDateTime = function () {
+exports.timeNow = function () {
 	let d = new Date();
-	let f = d.getFullYear() + "." + num.withZero(d.getMonth() + 1) + "." + num.withZero(d.getDate()) + ", "
-	+ num.withZero(d.getHours()) + ":" + num.withZero(d.getMinutes()) + ":" + num.withZero(d.getSeconds());
-	return f;
+	return d.getFullYear() + "."
+		+ num.withZero(d.getMonth() + 1) + "."
+		+ num.withZero(d.getDate()) + ", "
+		+ num.withZero(d.getHours()) + ":"
+		+ num.withZero(d.getMinutes()) + ":"
+		+ num.withZero(d.getSeconds());
 };
+exports.hhmm = () => timeNow().slice(0,-3)
